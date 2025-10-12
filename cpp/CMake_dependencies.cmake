2ac640aaad87ec4fc8c0033054c04bb10fba8658
Michal Lurie <Michal.Lurie@gmail.com>
2025-10-12 13:26:02 +0200

    chore: cleanup before merge

--- cpp/CMake_dependencies.cmake
+++ cpp/CMake_dependencies.cmake
@@ -3,6 +3,3 @@ include(cmake/CPM.cmake)
 CPMAddPackage(
     URI "gh:InCom-0/icu-cpm#cpm"
 )
-
-add_library(ICU::uc ALIAS icu)  # this alias is needed by harfbuzz
-add_library(icuuc ALIAS icu)  # this alias is needed by sfntly
